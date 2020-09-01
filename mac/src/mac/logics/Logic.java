a7dcf383cc33703d2c7be19bc23143cb1ee73022
梶ヶ谷篤哉 <kajigayaatsuya@kajigayaatsuyanoMacBook-Air.local>
2020-09-01 21:36:13 +0900

    java練習問題

---
+++ mac/src/mac/logics/Logic.java
@@ -0,0 +1,8 @@
+package mac.logics;
+
+public class Logic {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+
+}
---
+++ mac/src/mac/main/Main.java
@@ -0,0 +1,13 @@
+package mac.main;
+import mac.logics.Logic;
+
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 40;
+		int total = Logic.tasu(a, b);
+		System.out.println ("合計" + total);
+	}
+
+}
