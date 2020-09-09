4f3215e049d41c22c5809bf3d76633cd7f565988
梶ヶ谷篤哉 <kajigayaatsuya@kajigayaatsuyanoMacBook-Air.local>
2020-09-09 11:31:08 +0900

    インスタンス

---
+++ instance/Cleric.java
@@ -0,0 +1,23 @@
+public class Cleric {
+	String name;
+	int hp = 50;
+	final int MAX_HP = 50;
+	int mp = 10;
+	final int MAX_MP = 10;
+
+	public void selfaid() {
+		this.mp -= 5;
+		this.hp = this.MAX_HP;
+		System.out.println(this.name + "は、セルフエイドを唱えた");
+		System.out.println("HPを最大まで回復しました");
+	}
+
+	public void pray(int p) {
+		System.out.println(this.name + "は" + p + "秒間天に祈った！");
+		int recover = new java.util.Random().nextInt(3) + p;
+		int recoveractual = Math.min(this.MAX_MP - this.mp, recover);
+		this.mp += recoveractual;
+		System.out.println("MPが" + recoveractual + "回復した");
+		System.out.println("最終MPは" + this.mp + "でした");
+	}
+}
