6bdee42fa86a5b51284be12a8d1a504357dbb159
梶ヶ谷篤哉 <cb650r.477@gmail.com>
2020-09-23 22:31:50 +0900

    等価と等値

---
+++ APIgroup/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		Hero h2 = new Hero();
+		h2.name = "ミナト";
+		h2.hp = 100;
+		if (h.equals(h2) == true) {
+			System.out.println("同じ内容です");
+		} else {
+			System.out.println("違う内容です");
+		}
+		System.out.println(h.toString());
+	}
+}
