b0d36112868ed44c261f897f1a91ce55b83937ff
梶ヶ谷篤哉 <cb650r.477@gmail.com>
2020-09-27 15:26:55 +0900

    文字列の書式整形

--- Stringprocessing/Main.java
+++ Stringprocessing/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		String s1 = "java and javaScript";
+
 		if (s1.contains("java")) {
 			System.out.println("文字列s1は、javaを含んでいます");
 		}
@@ -16,5 +17,27 @@ public class Main {
 		System.out.println("文字列s1の4~8文字目は" + s1.substring(3, 8));
 
 		System.out.println("大文字を小文字に変換" + s1.toLowerCase());
+
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 10; i++) {
+			sb.append("java");
+		}
+		String s = sb.toString();
+		System.out.println(s);
+
+
+		String abc = "abc,def:ghi";
+
+		String[] words = abc.split("[,:]");
+		for (String w : words) {
+			System.out.print(w + "->");
+		}
+
+		String w1 = abc.replaceAll("[beh]", "x");
+		System.out.println(w1);
+
+		System.out.printf("製品番号%s-%02d" , "SUV" , 3);
 	}
 }
