4dcfd65d7084105694767942f0140bc1b1c65885
梶ヶ谷篤哉 <cb650r.477@gmail.com>
2020-09-28 22:36:13 +0900

    日付と時間の扱い

--- Stringprocessing/Main.java
+++ Stringprocessing/Main.java
@@ -1,3 +1,8 @@
+import java.util.Date;
+import java.util.Calendar;
+import java.time.*;
+import java.time.format.*;
+
 public class Main {
 	public static void main(String[] args) {
 		String s1 = "java and javaScript";
@@ -39,5 +44,75 @@ public class Main {
 		System.out.println(w1);
 
 		System.out.printf("製品番号%s-%02d" , "SUV" , 3);
+
+
+		Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+		Date past = new Date(1601295702320L);
+		System.out.println(past);
+
+
+		Calendar c = Calendar.getInstance();
+		c.set(2019,8,22,1,23,45);
+		c.set(Calendar.MONTH, 9);
+		Date d = c.getTime();
+		System.out.println(d);
+		c.setTime(now);
+		int y = c.get(Calendar.YEAR);
+		System.out.println("今年は" + y + "年です");
+
+
+		Instant i1 = Instant.now();
+		System.out.println(i1);
+
+		ZonedDateTime z1 = ZonedDateTime.now();
+		System.out.println(z1);
+
+		LocalDateTime l1 = LocalDateTime.now();
+		System.out.println(l1);
+
+
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate ldate = LocalDate.parse("2020/09/22", fmt);
+
+		// １０００日後を計算する
+		LocalDate ldatep = ldate.plusDays(1000);
+		String str = ldatep.format(fmt);
+		System.out.println("1000日後は" + str);
+
+		// 現在日時との比較
+		LocalDate now2 = LocalDate.now();
+		if (now2.isAfter(ldate)) {
+			System.out.println("1000日後は過去日付です");
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
