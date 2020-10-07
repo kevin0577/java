2d08ad5555c05389ef9f8e393daf49007a77e1c9
梶ヶ谷篤哉 <cb650r.477@gmail.com>
2020-10-07 12:42:10 +0900

    コレクション２

--- collection/Main.java
+++ collection/Main.java
@@ -14,5 +14,28 @@ public class Main {
 			Integer e = it.next();
 			System.out.println(e);
 		}
+
+		Set<String> colors = new TreeSet<String>();
+		colors.add("赤");
+		colors.add("青");
+		colors.add("黄");
+		for (String s : colors) {
+			System.out.print(s + ",");
+		}
+		System.out.println("色は" + colors.size() + "種類");
+
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		prefs.put("京都府", 1000);
+		prefs.put("東京都", 3000);
+		prefs.put("熊本県", 2000);
+		int tokyo = prefs.get("東京都");
+		System.out.println("東京の人口は" + tokyo);
+		prefs.put("熊本県", 2001);
+		int kumamoto = prefs.get("熊本県");
+		System.out.println("熊本の人口は" + kumamoto);
+		for (String key : prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(key + value);
+		}
 	}
 }
