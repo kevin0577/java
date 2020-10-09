fa9382340824170ea54570767b513e3609436b63
梶ヶ谷篤哉 <cb650r.477@gmail.com>
2020-10-09 21:00:31 +0900

    例外２

--- exception/Main.java
+++ exception/Main.java
@@ -2,10 +2,21 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) {
+		FileWriter fw = null;
 		try {
-			FileWriter fw = new FileWriter("data.txt");
-		} catch (IOException e) {
+			fw = new FileWriter("data.txt");
+			fw.write("hello");
+		} catch (Exception e) {
 			System.out.println("エラーが発生しました");
+		} finally {
+			if (fw != null) {
+			try {
+			fw.close();
+			} catch (IOException e) {
+				;
+			}
+
+			}
 		}
 	}
 }
